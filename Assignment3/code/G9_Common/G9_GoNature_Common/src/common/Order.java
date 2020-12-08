b2b7aef460c30c2b96365697a36adfc79e14535e
nastyak6 <nastyak6@walla.com>
2020-12-08 14:37:14 +0200

    sf
    sd

--- Assignment3/code/G9_Common/G9_GoNature_Common/src/common/Order.java
+++
@@ -1,7 +0,0 @@
-package common;
-
-public interface Order {
-	
-	public void makeAnOrder();
-	
-}
---
+++ Assignment3/code/G9_Common/G9_GoNature_Common/src/orderData/DateAndTime.java
@@ -0,0 +1,24 @@
+package orderData;
+
+public class DateAndTime {
+
+	String day;
+	String time;
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+}
