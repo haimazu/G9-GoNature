e91941189eb13ef8bfafddcc63370349b35d4fa5
Roi-Amar <54456400+Roi-Amar@users.noreply.github.com>
2020-12-08 10:01:33 +0200

    Update Test.java

--- Assignment3/code/G9_Client/G9_GoNature_Client/src/application/Test.java
+++ Assignment3/code/G9_Client/G9_GoNature_Client/src/application/Test.java
@@ -4,5 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("wow its working!");
+		
+		System.out.println("after saved");
 	}
 }
