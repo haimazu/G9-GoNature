f89ebb059bef1329a51c6075b1551b0b883c0a26
Roi-Amar <54456400+Roi-Amar@users.noreply.github.com>
2020-12-15 22:19:33 +0200

    .

--- Assignment3/code/G9_Server/G9_GoNature_Server/src/server/NewOrder.java
+++ Assignment3/code/G9_Server/G9_GoNature_Server/src/server/NewOrder.java
@@ -67,7 +67,8 @@ public class NewOrder {
 			// no parks in DB
 			answer.add(new ArrayList<String>(Arrays.asList("Failed")));
 		} else {
-			answer.add(queryData.get(0));
+			
+			answer.add(new ArrayList<String>(Arrays.asList(queryData.get(0).get(0),queryData.get(1).get(0),queryData.get(2).get(0))));
 		}
 		System.out.println(answer);
 		EchoServer.sendToMyClient(answer, client);
