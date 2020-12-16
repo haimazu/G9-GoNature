2fb84e6788517d42dc5267ed1840489455e186f4
Roi-Amar <54456400+Roi-Amar@users.noreply.github.com>
2020-12-16 17:00:27 +0200

    .

--- Assignment3/code/G9_Server/G9_GoNature_Server/src/server/WaitingList.java
+++ Assignment3/code/G9_Server/G9_GoNature_Server/src/server/WaitingList.java
@@ -1,5 +1,58 @@
 package server;
 
-public class WaitingList {
+import java.util.ArrayList;
 
+import ocsf.server.ConnectionToClient;
+import orderData.Order;
+
+public class WaitingList {
+	
+	//input: 
+	//
+	//output:
+	public void enterTheWaitList(ArrayList<Object> recived, ConnectionToClient client){
+		ArrayList<Object> answer = new ArrayList<Object>();
+		answer.add(recived.get(0));
+		Order data = (Order) recived.get(1); // credit card object received
+		
+		ArrayList<String> query = new ArrayList<String>();
+		//query.add("insert"); // command
+	//	query.add("orders"); // table name
+		//query.add(toStringForReservation(data)); // values in query format
+
+		if (MySQLConnection.insert(query))
+			answer.add(true);
+		else
+			answer.add(false);
+
+		EchoServer.sendToMyClient(answer, client);
+	}
+	
+	//input: 
+	//
+	//output:
+	public boolean checkForAvilableSpots() {
+		return true;
+	}
+	
+	//input: 
+	//
+	//output:
+	public void pullFromWaitList() {
+		
+	}
+	
+	//input: 
+	//
+	//output:
+	public void confirmAndOrder() {
+		
+	}
+	
+	//input: 
+	//
+	//output:
+	public void nextInLine() {
+		
+	}
 }
