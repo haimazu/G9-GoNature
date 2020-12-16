fb8b716d015c9019770edee640984b5f49c0d6a4
haimazu <36477174+haimazu@users.noreply.github.com>
2020-12-16 16:53:12 +0200

    Update ExistingOrderCheck.java

--- Assignment3/code/G9_Server/G9_GoNature_Server/src/server/ExistingOrderCheck.java
+++ Assignment3/code/G9_Server/G9_GoNature_Server/src/server/ExistingOrderCheck.java
@@ -38,6 +38,31 @@ public class ExistingOrderCheck {
 		EchoServer.sendToMyClient(answer, client);
 	}
 	
+	public static void getOrderDetailsByIdOrMemberId(ArrayList<Object> recived, ConnectionToClient client) {
+		// query
+		ArrayList<Object> answer = new ArrayList<Object>();
+		// the service name : existingOrderCheck
+		answer.add(recived.get(0));
+		// the data that sent from the client
+		// cell 0: orderNumber
+		ArrayList<String> data = (ArrayList<String>) recived.get(1);
+
+		ArrayList<String> query = new ArrayList<String>();
+		query.add("select"); // command
+		query.add("orders"); // table name
+		query.add("*"); // columns to select from
+		query.add("WHERE ID = '" + data.get(0) + "' OR memberId = '" + data.get(0) + "'"); // condition
+		query.add("10"); // how many columns returned
+
+		ArrayList<ArrayList<String>> queryData = MySQLConnection.select(query);
+		if (queryData.isEmpty()) {
+			answer.add(new ArrayList<String>(Arrays.asList("No such order")));
+		} else {
+			answer.add(queryData.get(0));
+		}
+		EchoServer.sendToMyClient(answer, client);
+	}
+	
 	public static void getOrderDetailsByOrderNumber(ArrayList<Object> recived, ConnectionToClient client) {
 		// query
 		ArrayList<Object> answer = new ArrayList<Object>();
