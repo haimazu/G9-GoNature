9ccf38e10cd8e63fbea399dbcf04b541277a8186
Roi-Amar <54456400+Roi-Amar@users.noreply.github.com>
2020-12-17 18:57:30 +0200

    Update Order.java

--- Assignment3/code/G9_Common/G9_GoNature_Common/src/orderData/Order.java
+++ Assignment3/code/G9_Common/G9_GoNature_Common/src/orderData/Order.java
@@ -1,45 +1,45 @@
 package orderData;
 
+import java.util.ArrayList;
+
 public class Order {
 
 	int orderNumber;
 	int visitorsNumber;
 	String orderEmail;
+	String orderPhone; 
 	OrderType orderType;
 	double price;
-	int percent;
-	double totalPrice;
-	String parkname;
+	String parkName;
 	String arrivedTime;
-	String memberId=null;
-	int ID=0;
+	String memberId;
+	String ID;
 
-
-	/**
-	 * @param visitorsNumber
-	 * @param orderEmail
-	 * @param parkname
-	 * @param arrivedTime
-	 * @param memberId
-	 * @param iD
-	 */
-	public Order(int visitorsNumber, String orderEmail, String parkname, String arrivedTime, String memberId,
-			int iD) {
-		super();
+	public Order(int orderNumber, int visitorsNumber,String orderEmail,String orderPhone, OrderType orderType,double price,String parkName,String arrivedTime,String memberId,String ID) {
+		this.orderNumber = orderNumber;
 		this.visitorsNumber = visitorsNumber;
 		this.orderEmail = orderEmail;
-		this.parkname = parkname;
+		this.orderPhone = orderPhone;
+		this.orderType = orderType;
+		this.price = price;
+		this.parkName = parkName;
 		this.arrivedTime = arrivedTime;
 		this.memberId = memberId;
-		ID = iD;
+		this.ID = ID;
 	}
 
-	public int getID() {
-		return ID;
-	}
-
-	public void setID(int iD) {
-		ID = iD;
+	public Order(ArrayList<String> orderFromDB) {
+		this.orderNumber = Integer.parseInt(orderFromDB.get(0));
+		this.visitorsNumber = Integer.parseInt(orderFromDB.get(1));
+		this.orderEmail = orderFromDB.get(2);
+		this.orderPhone = orderFromDB.get(3);
+		this.orderType = OrderType.valueOf(orderFromDB.get(4));
+		this.price = Double.parseDouble(orderFromDB.get(5));
+		this.parkName = orderFromDB.get(6);
+		this.arrivedTime = orderFromDB.get(7);
+		this.memberId = orderFromDB.get(8);
+		this.ID = orderFromDB.get(9);
+		// TODO Auto-generated constructor stub
 	}
 
 	public int getOrderNumber() {
@@ -66,6 +66,14 @@ public class Order {
 		this.orderEmail = orderEmail;
 	}
 
+	public String getOrderPhone() {
+		return orderPhone;
+	}
+
+	public void setOrderPhone(String orderPhone) {
+		this.orderPhone = orderPhone;
+	}
+
 	public OrderType getOrderType() {
 		return orderType;
 	}
@@ -82,12 +90,12 @@ public class Order {
 		this.price = price;
 	}
 
-	public String getParkname() {
-		return parkname;
+	public String getParkName() {
+		return parkName;
 	}
 
-	public void setParkname(String parkname) {
-		this.parkname = parkname;
+	public void setParkName(String parkName) {
+		this.parkName = parkName;
 	}
 
 	public String getArrivedTime() {
@@ -106,4 +114,24 @@ public class Order {
 		this.memberId = memberId;
 	}
 
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+	
+	public String toStringForDB() {
+		return "'" + getOrderNumber() + "','"
+				+ getVisitorsNumber() + "','"
+				+ getOrderEmail() + "','"
+				+ getOrderPhone() + "','"
+				+ getOrderType().toString().toLowerCase() + "','"
+				+ getPrice() + "','"
+				+ getParkName() + "','"
+				+ getArrivedTime() + "','"
+				+ getMemberId() + "'"
+				+ getID() + "'";
+	}
 }
