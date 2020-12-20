d9b1600a73c4f60fc97894c92ddc38dde9a38802
Nastya <nastyak6@gmail.com>
2020-12-20 21:01:19 +0200

    1
    1

--- Assignment3/code/G9_Server/G9_GoNature_Server/src/server/PaymentData.java
+++ Assignment3/code/G9_Server/G9_GoNature_Server/src/server/PaymentData.java
@@ -8,42 +8,32 @@ import ocsf.server.ConnectionToClient;
 import orderData.Order;
 
 public class PaymentData {
-	
-	
-	//input: ArrayList<Object>=> cell[0] function name
-	//                           cell[1] order object
-	//                           cell[2] payment type in string format
-	//                           cell[3] credit card object if payment type is credit card
+
+	// input: ArrayList<Object>=> cell[0] function name
+	// cell[1] order object
+	// cell[2] payment type in string format
+	// cell[3] credit card object if payment type is credit card
 	//
-	//output: ArrayList<Object>=> cell[0] function name
-	//                           cell[1] T\F if input to DB was success
+	// output: ArrayList<Object>=> cell[0] function name
+	// cell[1] T\F if input to DB was success
 	public static void PaymentInsertData(ArrayList<Object> recived, ConnectionToClient client) {
-		
+
 		WaitingList a = new WaitingList();
 		ArrayList<Object> answer = new ArrayList<Object>();
 		answer.add(recived.get(0));
-		Order data=(Order)recived.get(1);
-		if((String)recived.get(2)=="CreditCard") {
-			CreditCard cc=(CreditCard)recived.get(3);
-			if(creditCardSave(cc,data))
-				answer.add(true);
-			else
-				answer.add(false);
-		}
-		else {
-			answer.add(true);
-			EchoServer.sendToMyClient(answer, client);
-		}
+		Order data = (Order) recived.get(1);
+		CreditCard cc = (CreditCard) recived.get(3);
+		answer.add(creditCardSave(cc, data)); //T\F
+		EchoServer.sendToMyClient(answer, client);
 	}
-	
-	
+
 	// input: object credit card, object order
 	//
-	//output: T\F if success
+	// output: T\F if success
 	public static Boolean creditCardSave(CreditCard cc, Order data) {
-		
+
 		cc.setOrderNumber(data.getOrderNumber());
-		
+
 		ArrayList<String> query = new ArrayList<String>();
 		query.add("insert"); // command
 		query.add("creditcard"); // table name
@@ -54,17 +44,13 @@ public class PaymentData {
 		} else {
 			return false;
 		}
-		
+
 	}
 
-	
-	//in use for CreditCardSave query
-		public static String toStringForCreditCardSave(CreditCard data) {
-			return "'" + data.getCardNumber() + "','" 
-					+ data.getCardHolderName() + "','"
-					+ data.getExpirationDate() + "','" 
-					+ data.getCvc() +"','"
-					+data.getOrderNumber()+ "'";
-		}
-	
+	// in use for CreditCardSave query
+	public static String toStringForCreditCardSave(CreditCard data) {
+		return "'" + data.getCardNumber() + "','" + data.getCardHolderName() + "','" + data.getExpirationDate() + "','"
+				+ data.getCvc() + "','" + data.getOrderNumber() + "'";
+	}
+
 }
