83428fef5a8a0c134f1df62fcb7e8b9c4228fd3f
Roi-Amar <54456400+Roi-Amar@users.noreply.github.com>
2020-12-22 23:35:37 +0200

    Update Simulations.java

--- Assignment3/code/G9_Server/G9_GoNature_Server/src/simulations/Simulations.java
+++ Assignment3/code/G9_Server/G9_GoNature_Server/src/simulations/Simulations.java
@@ -39,7 +39,37 @@ public class Simulations {
 		return null;
 	}
 	
+	private static void reader(MessageSimulation message) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("\nTo: " + message.getTo());
+		if(message.getMessage().equals("waitingList notification")) {
+			do {
+			System.out.print("== waitlist notification == \n");
+			System.out.print("== please choose one option == \n");
+			System.out.print(" 1. ill make it   \n");
+			System.out.print(" 2. give up your spot   \n");
+			System.out.print("Enter your choice: ");
+			br = new BufferedReader(new InputStreamReader(System.in));
+			choice = Integer.parseInt(br.readLine());
+			if (choice==1)
+				System.out.println("im in!");
+				//replay - activate the function in the server
+			else if (choice==2)
+				System.out.println("im out");
+				//replay give out spot
+			} while (!(choice == 1 || choice == 2));
+		} else {
+			System.out.println(message.getMessage());
+			System.out.println("\n");
+		}
+	}
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		//tests only
+		recivedMails.add(new MessageSimulation(555, "messege!", "demo@simulation.com"));
+		recivedSms.add(new MessageSimulation(555, "waitingList notification", "0501234567"));
+		//remove when done playing
+		
 		System.out.print("========= Simulation ============ \n");
 		System.out.print("Enter orderNumber: ");
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -57,22 +87,22 @@ public class Simulations {
 				MessageSimulation message = CheckMailSimulation(orderNum);
 				if (message!=null) {
 					System.out.println("new mail for order " + orderNum );
-					if(message.getMessage().equals(""))
-					System.out.print("====  ==== \n");
-					System.out.print(" 1. Check Mail   \n");
+					reader(message);
+					recivedMails.remove(message);
 				}
 				else
-					System.out.println("no new mail! for order " + orderNum );
+					System.out.println("no new mail for order " + orderNum +"\n");
 				}
 				break;
 			case 2: {
 				MessageSimulation message = CheckSmsSimulation(orderNum);
 				if (message!=null) {
 					System.out.println("new sms for order " + orderNum );
-					
+					reader(message);
+					recivedSms.remove(message);
 				}
 				else
-					System.out.println("no sms mail! for order " + orderNum );
+					System.out.println("no new sms for order " + orderNum +"\n");
 				}
 				break;
 			case 0: {
