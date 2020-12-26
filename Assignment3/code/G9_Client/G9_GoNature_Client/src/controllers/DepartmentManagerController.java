4379cefc328aa75400a27506bd01850bca00392f
barKatz0610 <73135733+barKatz0610@users.noreply.github.com>
2020-12-26 12:04:46 +0200

    new

--- Assignment3/code/G9_Client/G9_GoNature_Client/src/controllers/DepartmentManagerController.java
+++ Assignment3/code/G9_Client/G9_GoNature_Client/src/controllers/DepartmentManagerController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXTextField;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,13 +14,104 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class DepartmentManagerController implements Initializable {
+
 	@FXML
-	private Label lblFirstNameTitle;
+	private Button btnDashboard;
+
+	@FXML
+	private Button btnVisitsReport;
+
+	@FXML
+	private Button btnCancelsReport;
+
+	@FXML
+	private Button btnSettings;
+
+	@FXML
+	private Label lblTitle;
+
 	@FXML
 	private Button btnLogout;
+
+	@FXML
+	private Label lblFirstNameTitle;
+
+	@FXML
+	private StackPane pnStack;
+
+	@FXML
+	private Pane pnSettings;
+
+	@FXML
+	private Pane pnCancels;
+
+	@FXML
+	private Pane pnVisits;
+
+//    @FXML
+//    private BarChart<?, ?> visitsChart;
+
+//    @FXML
+//    private CategoryAxis xAxis;
+//
+//    @FXML
+//    private NumberAxis yAxis;
+
+	@FXML
+	private Button btnReload;
+
+	@FXML
+	private Button btnLoad;
+
+	@FXML
+	private Button btnPrevPark;
+
+	@FXML
+	private Button btnNextPark;
+
+	@FXML
+	private Pane pnDashboard;
+
+//    @FXML
+//    private TableView<?> tableDep;
+
+	@FXML
+	private Label LabelCount;
+
+	@FXML
+	private Button btnReload1;
+
+	@FXML
+	private JFXTextField txtDiscount;
+
+	@FXML
+	private JFXTextField txtDates;
+
+	@FXML
+	private Button btnApproveDis;
+
+	@FXML
+	private JFXTextField txtMaxVisitor;
+
+	@FXML
+	private JFXTextField txtParkNameVis;
+
+	@FXML
+	private JFXTextField txtParkNameDis;
+
+	@FXML
+	private Button btnDisaproveDis;
+
+	@FXML
+	private Button btnApproveVis;
+
+	@FXML
+	private Button btnDisaproveVis;
 
 	private static String firstName;
 
@@ -35,6 +128,26 @@ public class DepartmentManagerController implements Initializable {
 
 	public static void setFirstName(String firstName) {
 		DepartmentManagerController.firstName = firstName;
+	}
+
+	@FXML
+	void seeAll(ActionEvent event) {
+
+	}
+
+	@FXML
+	void approve(ActionEvent event) {
+
+	}
+
+	@FXML
+	void btnApproveDis(ActionEvent event) {
+
+	}
+
+	@FXML
+	void disapprove(ActionEvent event) {
+
 	}
 
 	@Override
