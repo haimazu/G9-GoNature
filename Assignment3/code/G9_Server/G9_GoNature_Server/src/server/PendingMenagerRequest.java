62532bea5441c0e561ee35933fa2644063d78f4c
Nastya <nastyak6@gmail.com>
2020-12-26 16:14:17 +0200

    d
    d

--- Assignment3/code/G9_Server/G9_GoNature_Server/src/server/PendingMenagerRequest.java
+++ Assignment3/code/G9_Server/G9_GoNature_Server/src/server/PendingMenagerRequest.java
@@ -9,7 +9,7 @@ public class PendingMenagerRequest {
 
 	// input: ArrayList<Object>: cell[0] name
 	// cell[1] ManagerRequest object
-	// 
+	//
 	// output:
 	public static void OverallVisitorsReport(ArrayList<Object> recived, ConnectionToClient client) {
 		ArrayList<Object> answer = new ArrayList<Object>();
@@ -17,32 +17,38 @@ public class PendingMenagerRequest {
 		answer.add(recived.get(0));
 		ManagerRequest mr = (ManagerRequest) recived.get(1);
 		// select pending requests and to check if not more than 1 in every type
-		ArrayList<String> query = new ArrayList<String>();
-		query.add("select"); // command
-		query.add("pendingmanagerrequests"); // table name
-		query.add("*"); // columns to select from
-		query.add("WHERE requesttype='" + mr.getRequesttype() + "' AND employeeID='" + mr.getEmployeeID() + "'");
+		ArrayList<String> query1 = new ArrayList<String>();
+		query1.add("select"); // command
+		query1.add("pendingmanagerrequests"); // table name
+		query1.add("*"); // columns to select from
+		query1.add("WHERE requesttype='" + mr.getRequesttype() + "' AND employeeID='" + mr.getEmployeeID() + "'");
 
-		if (!query.isEmpty()) {
-			System.out.println("already has this type of request");
+		if (!query1.isEmpty()) {
+			System.out.println("already has this type of request for this employee");
 			answer.add(false);
 		}
 		// insert if possible
 		else {
+			ArrayList<String> query2 = new ArrayList<String>();
+			query2.add("insert"); // command
+			query2.add("orders"); // table name
+			query2.add(toStringForDB(mr)); // values in query format
+			if (!MySQLConnection.insert(query2)) {
+				answer.add(false);
+				System.out.println("failed to insert");
+			} else {
+				answer.add(true);
+
+			}
 
 		}
-
+		EchoServer.sendToMyClient(answer, client);
 	}
 
 	public static String toStringForDB(ManagerRequest mr) {
-		return "'" + mr.getEmployeeID() + "','" 
-				+ mr.getRequesttype() + "','" 
-				+ mr.getMaxCapacity() + "','"
-				+ mr.getOrdersCapacity() + "','" 
-				+ mr.getDiscount() + "','" 
-				+ mr.getFromDate() + "','"
-				+ mr.getToDate() + "','" 
-				+ mr.getParkName() + "'";
+		return "'" + mr.getEmployeeID() + "','" + mr.getRequesttype() + "','" + mr.getMaxCapacity() + "','"
+				+ mr.getOrdersCapacity() + "','" + mr.getDiscount() + "','" + mr.getFromDate() + "','" + mr.getToDate()
+				+ "','" + mr.getParkName() + "'";
 	}
 
 }
