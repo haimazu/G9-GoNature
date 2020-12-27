c653a88e3f8830ba87b9fba3bb4fb0d05f68e09b
haimazu <36477174+haimazu@users.noreply.github.com>
2020-12-27 16:26:38 +0200

    s

--- Assignment3/code/G9_Client/G9_GoNature_Client/src/controllers/DepartmentManagerController.java
+++ Assignment3/code/G9_Client/G9_GoNature_Client/src/controllers/DepartmentManagerController.java
@@ -2,6 +2,8 @@ package controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
@@ -29,64 +31,52 @@ import javafx.stage.Stage;
 import reportData.TableViewSet;
 
 public class DepartmentManagerController implements Initializable {
-
+	/***** Side Bar *****/
 	@FXML
 	private Button btnDashboard;
-
 	@FXML
 	private Button btnVisitsReport;
-
 	@FXML
 	private Button btnCancelsReport;
-
 	@FXML
 	private Button btnSettings;
 
+	/***** Top Title *****/
 	@FXML
 	private Label lblTitle;
-
 	@FXML
 	private Label lblFirstNameTitle;
 
+	/***** Panels *****/
 	@FXML
 	private StackPane pnStack;
-
+	@FXML
+	private Pane pnDashboard;
+	@FXML
+	private Pane pnVisits;
+	@FXML
+	private Pane pnCancels;
 	@FXML
 	private Pane pnSettings;
 
-	@FXML
-	private Pane pnCancels;
-
-	@FXML
-	private Pane pnVisits;
-
-	@FXML
-	private Pane pnDashboard;
-
+	/***** Dashboard *****/
 	@FXML
 	private TableView<TableViewSet> tableDep;
-
 	@FXML
 	private TableColumn<TableViewSet, String> parkName;
-
 	@FXML
 	private TableColumn<TableViewSet, String> requestType;
-
 	@FXML
 	private TableColumn<TableViewSet, String> requestDetails;
-
 	@FXML
 	private TableColumn<TableViewSet, CheckBox> checkBox;
-
 	@FXML
 	private Label LabelCount;
-
 	@FXML
 	private Button btnApprove;
-
 	@FXML
 	private Button btnDisaprove;
-	
+
 	/***** Cancel Reports *****/
 	@FXML
 	private BarChart<?, ?> bcCancells;
@@ -102,14 +92,14 @@ public class DepartmentManagerController implements Initializable {
 	private Button btnShow;
 	@FXML
 	private Button btnLogout;
-	
-	/***** Cancel Reports Variables *****/
-	private static ArrayList<Object> cancelReportsDetails = new ArrayList<>();
 
 	private static String firstName;
 
-	private static ArrayList<ArrayList<String>> DBList = new ArrayList<>();
+	/***** Cancel Reports Variables *****/
+	private static ArrayList<Object> cancelReportsDetails = new ArrayList<>();
 
+	/***** Dashboard Variables *****/
+	private static ArrayList<ArrayList<String>> DBList = new ArrayList<>();
 	private int count = 0;
 
 	public static ArrayList<ArrayList<String>> getDBList() {
@@ -118,6 +108,36 @@ public class DepartmentManagerController implements Initializable {
 
 	public static void setDBList(ArrayList<ArrayList<String>> dBList) {
 		DBList = dBList;
+	}
+
+	@FXML
+	public void handleSideBar(ActionEvent event) {
+		if (event.getSource() == btnDashboard) {
+			lblTitle.setText("Dashboard");
+			pnDashboard.toFront();
+			setButtonPressed(btnDashboard);
+			setButtonReleased(btnVisitsReport, btnCancelsReport, btnSettings);
+		} else if (event.getSource() == btnVisitsReport) {
+			lblTitle.setText("Visits Report");
+			pnVisits.toFront();
+			setButtonPressed(btnVisitsReport);
+			setButtonReleased(btnDashboard, btnCancelsReport, btnSettings);
+		} else if (event.getSource() == btnCancelsReport) {
+			lblTitle.setText("Cancels Report");
+			pnCancels.toFront();
+			setButtonPressed(btnCancelsReport);
+			setButtonReleased(btnDashboard, btnVisitsReport, btnSettings);
+		} else if (event.getSource() == btnSettings) {
+			lblTitle.setText("Settings");
+			pnSettings.toFront();
+			setButtonPressed(btnSettings);
+			setButtonReleased(btnVisitsReport, btnCancelsReport, btnCancelsReport);
+		}
+	}
+
+	@FXML
+	void show(ActionEvent event) {
+		checkDate();
 	}
 
 	@FXML
@@ -186,6 +206,63 @@ public class DepartmentManagerController implements Initializable {
 		tableDep.getItems().add(new TableViewSet(al));
 	}
 
+	public void setButtonPressed(Button button) {
+		button.setStyle("-fx-background-color: transparent;\r\n" + "	-fx-border-color: brown;\r\n"
+				+ "	-fx-border-width: 0px 0px 0px 3px;");
+	}
+
+	public void setButtonReleased(Button button, Button button1, Button button2) {
+		button.setStyle("-fx-background-color: transparent;");
+		button1.setStyle("-fx-background-color: transparent;");
+		button2.setStyle("-fx-background-color: transparent;");
+	}
+
+	public void checkDate() {
+		ArrayList<String> data = new ArrayList<>();
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+		LocalDate from = dpFrom.getValue();
+		String fromFormat = dateTimeFormatter.format(from);
+
+		LocalDate to = dpTo.getValue();
+		String toFormat = dateTimeFormatter.format(to);
+
+		System.out.println(fromFormat);
+		System.out.println(toFormat);
+
+		// good
+		if (from.isBefore(to)) {
+			data.add(fromFormat);
+			data.add(toFormat);
+			sendToServerArrayList(data);
+		}
+	}
+
+	// ArrayList<String> data, sending to the server to get data
+	// input: [0] case name
+	// [1] month
+	// [2] year
+	// output: none
+	public void sendToServerArrayList(ArrayList<String> date) {
+		// Query
+		ArrayList<Object> msg = new ArrayList<Object>();
+		msg.add("getCancellationReports");
+		// add date
+		msg.add(date);
+		// set up all the order details and the payment method
+		ClientUI.sentToChatClient(msg);
+	}
+
+	// getting information from the server
+	// input: none
+	// output: list of cancelled orders:
+	// ArrayList<Object>: cell[0] case name
+	// cell[1] list of cancelled orders
+	// cell[2] list of dismissed orders
+	public static void receivedFromServerVisitorsPrice(ArrayList<Object> msgReceived) {
+		DepartmentManagerController.cancelReportsDetails = msgReceived;
+	}
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
@@ -215,5 +292,20 @@ public class DepartmentManagerController implements Initializable {
 		count = DBList.size();
 		LabelCount.setText(String.valueOf(count));
 		addData(DBList);
+		
+		/***** Cancel Reports *****/
+		dpFrom.setValue(LocalDate.now().withDayOfMonth(1));
+		// listener for updating the date
+		dpFrom.valueProperty().addListener((ov, oldValue, newValue) -> {
+			dpFrom.setValue(newValue);
+		});
+
+		// plusMonths(1) to get the next month
+		// withDayOfMonth(1) to get the first day
+		dpTo.setValue(dpFrom.getValue().plusMonths(1).withDayOfMonth(1));
+		// listener for updating the date
+		dpTo.valueProperty().addListener((ov, oldValue, newValue) -> {
+			dpTo.setValue(newValue);
+		});
 	}
 }
