8b3636fe9433076d559e81683a61a5a933201f58
Nastya <nastyak6@gmail.com>
2021-01-01 17:11:25 +0200

    r
    r

--- Assignment3/code/G9_Server/G9_GoNature_Server/src/server/Counter.java
+++ Assignment3/code/G9_Server/G9_GoNature_Server/src/server/Counter.java
@@ -9,12 +9,14 @@ class Counter {
 	private static int memCount;
 	private static int waitlistNum;
 	private static int cancelledCount;
+	private static int discountsIDcount;
 
 	private Counter() {
 		count = getLastNumber("orders", "orderNumber") + 1;
 		memCount = getLastNumber("member", "memberNumber") + 1;
 		waitlistNum = getLastNumber("waitinglist", "waitlistID") + 1;
 		cancelledCount = getLastNumber("canceledorders", "orderNumber") + 1;
+		discountsIDcount = getLastNumber("discounts", "requestID");
 	}
 
 	public static Counter getCounter() {
@@ -42,6 +44,10 @@ class Counter {
 		return cancelledCount++;
 	}
 
+	public int discountsIDcountNum() {
+		return discountsIDcount++;
+	}
+
 	private int getLastNumber(String tableName, String Col) {
 		ArrayList<String> query = new ArrayList<String>();
 		query.add("select");
@@ -51,7 +57,7 @@ class Counter {
 		query.add("1");
 		ArrayList<ArrayList<String>> lastNum = MySQLConnection.select(query);
 		if (lastNum.isEmpty())
-			return 0;
+			return 1111;
 		return Integer.parseInt(lastNum.get(0).get(0));
 	}
 }
