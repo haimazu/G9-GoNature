b00cdad2113b14f6fa8c4abf1fd5ab1c4f3cc636
RinatSt <rinat20968@gmail.com>
2021-01-01 22:54:30 +0200

    c

--- Assignment3/DB/OverallVisitorsReportQuery.sql
+++
@@ -1,4 +0,0 @@
-SELECT orderType, SUM(amountArrived), arrivedTime
- FROM g9_gonature.orders
- WHERE parkName='jurasic' AND arrivedTime BETWEEN '2020-12-28' AND '2021-01-03' AND amountArrived >0
- GROUP BY orderType;
