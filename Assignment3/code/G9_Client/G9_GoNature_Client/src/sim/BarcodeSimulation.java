7c924e062fa90d51af0088ee9c17416b4a62eb28
Roi-Amar <54456400+Roi-Amar@users.noreply.github.com>
2021-01-03 00:16:29 +0200

    simulatebarode

--- Assignment3/code/G9_Client/G9_GoNature_Client/src/sim/BarcodeSimulation.java
+++ Assignment3/code/G9_Client/G9_GoNature_Client/src/sim/BarcodeSimulation.java
@@ -1,29 +1,24 @@
 package sim;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.SplittableRandom;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class BarcodeSimulation {
 	private static BarcodeSimulation obj;
-	private static int choice;
-	private static int orderNum;
-	private static boolean listener;
-
+	private static ArrayList<Integer> pool;
+	
 	// SingletonBuilder
 	private BarcodeSimulation() {
-		listener = true;
-		// on start up do this
-	}
-
-	public int giveItToMe() {
-		listener = false;
-		while (!listener)
-			;
-		return orderNum;
+		pool.add(1133);
+		pool.add(1132);
+		pool.add(1121);
+		pool.add(1111);
+		pool.add(1234);
+		pool.add(5555);
+		pool.add(1128);
+		pool.add(1145);
+		pool.add(1155);
+		pool.add(1165);
 	}
 
 	// Singleton, create one instance only
@@ -35,64 +30,23 @@ public class BarcodeSimulation {
 		}
 		return obj;
 	}
-
-	public static void interactive() throws NumberFormatException, IOException {
-		listener = true;
-		System.out.print("========= Simulation ============ \n");
-		System.out.print("\nlistening for calls...\n");
-		while (listener) {
-			while (listener) {
-			}
-			do {
-				System.out.print("========= Barcode Simulation ========== \n");
-				System.out.print(" 1. Random Answer \n");
-				System.out.print(" 2. Manual Answer \n");
-				System.out.print(" 0. Exit \n");
-				System.out.print("Enter your choice: ");
-				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-				choice = Integer.parseInt(br.readLine());
-				switch (choice) {
-				case 1: {
-					orderNum = new SplittableRandom().nextInt(0, 1_001);
-					listener = true;
-					break;
-				}
-				case 2: {
-					System.out.print("Enter your choice: ");
-					br = new BufferedReader(new InputStreamReader(System.in));
-					orderNum = Integer.parseInt(br.readLine());
-					listener = true;
-					break;
-				}
-				case 0: {
-					System.out.println("see you soon!");
-					orderNum = 0;
-					System.out.print("\nlistening for calls...\n");
-					listener = true;
-					break;
-				}
-				default: {
-					System.out.print("\nbad choice, not available\n");
-					break;
-				}
-				}// end of switch
-
-			} while (choice != 0 || choice != 1 || choice != 2);
-
+	
+	public static Integer read(int key) {
+		switch (key) {
+		case 0:
+			return new SplittableRandom().nextInt(0, 1_001);
+		case 1:
+			return nextIn();
+		default:
+			return null;
 		}
 	}
-	public static void main(String[] args) {
-		ExecutorService exec = Executors.newSingleThreadExecutor();
-		exec.execute(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					getSim().interactive();
-				} catch (NumberFormatException | IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		});	
+	
+	public static Integer nextIn() {
+		if (pool.isEmpty())
+			return null;
+		Integer ret = pool.get(0);
+		pool.remove(ret);
+		return ret;
 	}
 }
