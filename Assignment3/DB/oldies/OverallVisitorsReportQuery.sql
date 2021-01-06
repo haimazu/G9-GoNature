a6fc66ee417d01d175dcc84a457ee88d4d401bdc
Nastya <nastyak6@gmail.com>
2021-01-06 20:21:55 +0200

    ע
    ע

---
+++ Assignment3/DB/oldies/OverallVisitorsReportQuery.sql
@@ -0,0 +1,4 @@
+SELECT  orderType, SUM(amountArrived), arrivedTime
+ FROM g9_gonature.orders
+ WHERE parkName='jurasic' AND arrivedTime BETWEEN '2020-12-28' AND '2021-01-03' AND amountArrived >0
+ GROUP BY DATE(arrivedTime), orderType;
