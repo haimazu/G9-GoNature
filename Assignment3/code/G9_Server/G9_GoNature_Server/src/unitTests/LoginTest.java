b081ee57a41442bbdfe3e06a336a708408cbc598
haimazu <36477174+haimazu@users.noreply.github.com>
2021-01-11 18:02:42 +0200

    unit tests

---
+++ Assignment3/code/G9_Server/G9_GoNature_Server/src/unitTests/LoginTest.java
@@ -0,0 +1,52 @@
+package unitTests;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import server.Login;
+
+class LoginTest {
+	private ArrayList<Object> received;
+	
+	private String expected = "Falied";
+	
+	@Mock
+	private Login mockLogin;
+	
+	// Executed before each test. It is used to prepare the test environment
+	@BeforeEach
+	void setUp() throws Exception {
+		received = new ArrayList<Object>();
+		
+		mockLogin = Mockito.mock(Login.class);
+		//mockLogin.login(received, client);
+		
+		//Mockito.when(Login.login(received, null));
+	}
+
+	// invalid username and password
+	
+	// valid username and invalid password
+	
+	// invalid username and valid password
+	
+	// valid username and password
+	
+	// the user is already logged in (the user must be exists in the system)
+	@Test
+	void test() {
+		
+		// username
+		received.add("Haim");
+		// password
+		received.add("12345679");	
+		expected = "Falied";
+		
+		//assertEquals(expected, MockLoginService.Login(received));
+		received.clear();
+	}
+}
